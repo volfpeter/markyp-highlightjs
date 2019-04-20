2d804efa71f5d7522656944c0f938e3037bdccd6
Péter Volf <peter@milinnovations.com>
2019-04-20 17:44:38 +0200

    bumped version to 0.1905.0 + added __all__

--- markyp_highlightjs/__init__.py
+++ markyp_highlightjs/__init__.py
@@ -16,7 +16,10 @@ __copyright__ = "Copyright 2019, Peter Volf"
 __email__ = "do.volfp@gmail.com"
 __license__ = "MIT"
 __url__ = "https://github.com/volfpeter/markyp-highlightjs"
-__version__ = "0.1904.0"
+__version__ = "0.1905.0"
+
+
+__all__ = ("CDN", "js", "themes", "highlight")
 
 
 class CDN(object):
