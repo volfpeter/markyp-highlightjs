3ae4a38267b733c8dbc4d8a538f86c64a4dd136e
Péter Volf <peter@milinnovations.com>
2019-06-08 09:36:30 +0200

    no message

--- markyp_highlightjs/__init__.py
+++ markyp_highlightjs/__init__.py
@@ -17,7 +17,7 @@ __copyright__ = "Copyright 2019, Peter Volf"
 __email__ = "do.volfp@gmail.com"
 __license__ = "MIT"
 __url__ = "https://github.com/volfpeter/markyp-highlightjs"
-__version__ = "0.1905.0"
+__version__ = "0.1906.0"
 
 
 __all__ = (
